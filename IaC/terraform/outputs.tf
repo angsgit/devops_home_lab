308d352880c9bcd62536b4cedbceea7e4d6534ec
Angad <angad@live.ie>
2025-10-28 16:55:38 +0000

    update files to catch public ip

--- IaC/terraform/outputs.tf
+++ IaC/terraform/outputs.tf
@@ -1,4 +1,4 @@
-output "instance_public_ip" {
-  description = "Public IP of the test EC2 instance"
+output "ec2_public_ip" {
+  description = "Public IP of the created EC2 instance"
   value       = aws_instance.test_vm.public_ip
 }
